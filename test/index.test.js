c8873d97d0612669ea42179f15a31cd4c3ae482a
Brian Zelip <bzelip@gmail.com>
2017-09-17 12:23:15 -0400

    install nyc for ava coverage reporting, cover all uncovered tests

--- test/index.test.js
+++ test/index.test.js
@@ -8,3 +8,21 @@ test('Check if getAmount is defined', t => {
 test('Check pennies', t => {
   t.true(getAmount('p') === 1);
 });
+
+test('Check nickels', t => {
+  t.true(getAmount('n') === 5);
+});
+
+test('Check dimes', t => {
+  t.true(getAmount('d') === 10);
+});
+
+test('Check quarters', t => {
+  t.true(getAmount('q') === 25);
+});
+
+test('Check error', t => {
+  t.throws(() => {
+    getAmount('x');
+  });
+});
